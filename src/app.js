c9e465f4fbd9459ee9bd36b7915df794951b9a4b
Agustin <agustin.villena@gmail.com>
2016-05-29 15:25:46 +0000

    test ticker green

--- src/app.js
+++ src/app.js
@@ -8,12 +8,20 @@ Time.prototype.tick = function(){
 
 function Ticker(someTime){
     this.time = someTime;
+    this.interval = null;
 }
 
 Ticker.prototype.start = function(){
-    
+    if(this.interval) return;
+    this.interval = setInterval(function(){
+        this.time.tick();    
+    }.bind(this),1000);   
 }
 
 Ticker.prototype.stop = function(){
-    
+    clearInterval(this.interval);
+    this.interval = null;
+}
+
+function stopwatchController(){
 }
